9d33af03fdcf43448de217370ecc441f7aef061e
Anoop George <anoopgeorge418@.com>
2024-10-16 09:41:52 +0530

    Learning From Basics

---
+++ Webscraping/Basics/wikiSpider/spiders/article.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class ArticleSpider(scrapy.Spider):
+    name = 'article'
+
+    def start_request(self):
+        urls =  [
+            'http://en.wikipedia.org/wiki/Python_'
+            '%28programming_language%29',
+            'https://en.wikipedia.org/wiki/Functional_programming',
+            'https://en.wikipedia.org/wiki/Monty_Python'
+        ]
+
+        return [scrapy.Request(url = url, callback = self.parse) for url in urls]
+
+    def parse(self, response):
+        url = response.url
+        title = response.css('h1::text').extract_first()
+        print('URL is: {}'.format(url))
+        print('Title is: {}'.format(title))
+
+# ArticleSpider().start_request()
+# ArticleSpider().parse()
